a6c3fbff1743f2cf9ad98fb11098664425936142
berkay3152 <95592702+berkay3152@users.noreply.github.com>
2025-11-11 08:47:05 +0300

    index.js

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const axios = require('axios');
 
-const SUPABASE_URL = process.env.SUPABASE_URL;
-const API_KEY = process.env.SUPABASE_API_KEY;
+const SUPABASE_URL = 'https://ftvfrivqwedvaptmxiqw.supabase.co/rest/v1/Gallery';
+const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ0dmZyaXZxd2VkdmFwdG14aXF3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIzNzEzMzMsImV4cCI6MjA3Nzk0NzMzM30.k6e4_PJCB2RPYmuKYqgzdrxQF-shXftcrP4kLhLtt1s';
 
 async function checkUpgrades() {
   const now = Math.floor(Date.now() / 1000);
 
   const { data } = await axios.get(SUPABASE_URL, {
     params: {
-      upgrade_end_timestamp: `lt.${now}`,
+      upgradeendtimestamp: `lt.${now}`,
       level: `not.is.null`
     },
     headers: {
